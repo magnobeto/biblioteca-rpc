40364e5eb443702302f2f54388d62ffe39159bf4
roberto.campos <roberto.magno@g.globo>
2025-05-19 13:54:32 -0300

    changing client examples

--- examples/client_example.py
+++ examples/client_example.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     client = RPCClient()
     math_stub = MathServiceStub(client)
 
-    print("Operação 34 + 8 = ", math_stub.add(34, 8))
-    print("Operação 55 - 11 = ", math_stub.sub(55, 11))
-    print("Operação 5 * 5 = ", math_stub.multiply(5, 5))
-    print("Operação 33 / 11 = ", math_stub.divide(33, 11))
+    print("Resultado de 34 + 8: ", math_stub.add(34, 8))
+    print("Resultado de 55 - 11: ", math_stub.sub(55, 11))
+    print("Resultado de 5 * 5:  ", math_stub.multiply(5, 5))
+    print("Resultado de 33 / 11:  ", math_stub.divide(33, 11))
